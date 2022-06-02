785f32b91e176a72b7412c3e8dda1fe190dcb4f7
Stefany Schiara <esquiara@gmail.com>
2022-06-02 16:39:27 -0300

    Formacao java alura

---
+++ src/main/resources/alura/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+package main.resources.alura;
+
+public class TestaPontoFlutuante{
+
+    public static void main(String[] args){
+
+        double salario = 1250.70;
+
+        System.out.println("meu salario é " + salario);
+        
+        double divisao = 5.0 / 2;
+        System.out.println("divisao: " + divisao);
+
+    }
+}
